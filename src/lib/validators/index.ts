84e57e2fa0215714ef457b15b995d53c83dbce79
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-12-11 14:58:38 +0530

    feat: update validator

--- src/lib/validators/index.ts
+++ src/lib/validators/index.ts
@@ -1,28 +1,4 @@
-abstract class Validator {
-  abstract validate(): Promise<Error | null>;
+export abstract class Validator<TInput, TOutput> {
+  abstract validate(input: TInput): Promise<Error | TOutput>;
   abstract getName(): string;
-}
-
-export class ValidationManager {
-  private validators: Validator[];
-
-  constructor(validators: Validator[]) {
-    this.validators = validators;
-  }
-
-  addValidator(validator: Validator) {
-    this.validators.push(validator);
-  }
-
-  async validate(): Promise<{ name: string; error: Error } | null> {
-    for (const validator of this.validators) {
-      const result = await validator.validate();
-
-      if (result) {
-        return { name: validator.getName(), error: result };
-      }
-    }
-
-    return null;
-  }
 }
