a2eed16af9ca3c7294c357dec6e9f91b4ce26b8b
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-01-06 22:00:48 +0530

    feat: handlers for health, allocate coin

---
+++ src/lib/server/handlers/allocate_coin.ts
@@ -0,0 +1,85 @@
+import type { Request } from 'express';
+import type { AllocateCoinResponse } from '../../../types';
+import { normalizeJSON, type Coin } from 'fuels';
+import type { SupabaseDB } from '../../db';
+import type { FuelClient } from '../../fuel';
+import { AllocateCoinResponseSchema } from '../../schema/api';
+import type { envSchema } from '../../schema/config';
+
+export const allocateCoinHandler = async (
+  req: Request,
+  res: AllocateCoinResponse
+) => {
+  // TODO: find a way to directly derive this from the typescript compiler, i.e avoid using `as`
+  const supabaseDB = req.app.locals.supabaseDB as SupabaseDB;
+  const fuelClient = req.app.locals.fuelClient as FuelClient;
+  const ENV = req.app.locals.ENV as Zod.infer<typeof envSchema>;
+
+  let coin: Coin | null = null;
+  let address: string | null = null;
+
+  while (!coin) {
+    address = await supabaseDB.getNextAccount();
+    if (!address) {
+      return res.status(404).json({ error: 'No unlocked account found' });
+    }
+
+    const result = await fuelClient.getCoin(address, ENV.MINIMUM_COIN_VALUE);
+    if (!result) {
+      await supabaseDB.setAccountNeedsFunding(address, true);
+      continue;
+    }
+
+    coin = result;
+  }
+
+  if (!address) {
+    return res.status(404).json({
+      error: 'No unlocked account found after multiple attempts',
+    });
+  }
+  if (!coin) {
+    return res
+      .status(404)
+      .json({ error: 'No unlocked coin found after multiple attempts' });
+  }
+
+  // Lock the account & the job for 30 seconds
+  const lockTimeStamp = new Date(Date.now() + 1000 * 30);
+
+  // Lock the account for 30 seconds
+  const lockError = await supabaseDB.lockAccount(address, lockTimeStamp);
+
+  if (lockError) {
+    console.error(lockError);
+    return res.status(500).json({ error: 'Failed to lock account' });
+  }
+
+  const { error: insertError, jobId } = await supabaseDB.insertNewJob(
+    address,
+    lockTimeStamp
+  );
+  if (insertError) {
+    console.error(insertError);
+    return res.status(500).json({ error: 'Failed to insert job' });
+  }
+
+  console.log('jobId', jobId);
+  // console.log('sent coin:', coin);
+
+  const {
+    success,
+    data: response,
+    error,
+  } = AllocateCoinResponseSchema.safeParse({
+    coin: normalizeJSON(coin),
+    jobId,
+  });
+
+  if (!success) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to allocate coin' });
+  }
+
+  res.status(200).send(response);
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,16 @@ export type JobStatus = 'pending' | 'completed' | 'timeout';
 
 export * from './database.types';
 export * from './api.types';
+
+// Define your app.locals interface
+interface AppLocals {
+  title: string;
+  user: {
+    id: number;
+    name: string;
+  };
+  config: {
+    apiKey: string;
+    environment: 'development' | 'production';
+  };
+}
