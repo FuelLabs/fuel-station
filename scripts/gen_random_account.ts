bccd8520bd6f8fd742c17e776b6090459541f1a8
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-01-07 15:55:11 +0530

    feat: sign handler

--- scripts/gen_random_account.ts
+++ scripts/gen_random_account.ts
@@ -2,7 +2,7 @@ import { Wallet } from 'fuels';
 
 // generates a random account and prints the private key and address
 const main = async () => {
-  const wallet = await Wallet.generate();
+  const wallet = Wallet.generate();
   console.log(`Private key: ${wallet.privateKey}`);
   console.log(`Address: ${wallet.address}`);
 };
