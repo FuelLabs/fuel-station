95c592c18ed64f9ea05e34752e07475a7a957999
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-01-09 15:06:26 +0530

    feat: method to prepare gasless transaction in gas station client

--- src/lib/client/index.ts
+++ src/lib/client/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Address, bn, type Coin, type ScriptTransactionRequest } from 'fuels';
+import { findInputCoinTypeCoin } from '../utils';
 
 export class gasStationClient {
   endpoint: string;
@@ -8,26 +9,64 @@ export class gasStationClient {
     this.endpoint = endpoint;
   }
 
-  async prepareGasLessTransaction(transaction: ScriptTransactionRequest) {
-    const { data } = await axios.post(`${this.endpoint}/allocate-coin`);
+  async prepareGaslessTransaction(transaction: ScriptTransactionRequest) {
+    const { data: allocateCoinResponseData } = await axios.post(
+      `${this.endpoint}/allocate-coin`
+    );
 
-    if (!data.coin) {
+    if (!allocateCoinResponseData.coin) {
       throw new Error('No coin found');
     }
 
-    if (!data.jobId) {
+    if (!allocateCoinResponseData.jobId) {
       throw new Error('No jobId found');
     }
 
+    const { coin, jobId } = allocateCoinResponseData;
+
     const gasCoin: Coin = {
-      id: data.coin.id,
-      amount: bn(data.coin.amount),
-      assetId: data.coin.assetId,
-      owner: Address.fromAddressOrString(data.coin.owner),
-      blockCreated: bn(data.coin.blockCreated),
-      txCreatedIdx: bn(data.coin.txCreatedIdx),
+      id: coin.id,
+      amount: bn(coin.amount),
+      assetId: coin.assetId,
+      owner: Address.fromAddressOrString(coin.owner),
+      blockCreated: bn(coin.blockCreated),
+      txCreatedIdx: bn(coin.txCreatedIdx),
     };
 
     transaction.addCoinInput(gasCoin);
+
+    const { data: maxValuePerCoinResponseData } = await axios.get(
+      `${this.endpoint}/metadata`
+    );
+
+    const maxValuePerCoin = bn(maxValuePerCoinResponseData.maxValuePerCoin);
+
+    transaction.addCoinOutput(
+      gasCoin.owner,
+      gasCoin.amount.sub(maxValuePerCoin),
+      gasCoin.assetId
+    );
+
+    const { data: signature } = await axios.post(`${this.endpoint}/sign`, {
+      request: transaction.toJSON(),
+      jobId,
+    });
+
+    const gasCoinInput = findInputCoinTypeCoin(
+      // @ts-ignore: This should work, although we need to change this util's interface
+      transaction,
+      gasCoin.owner,
+      gasCoin.assetId
+    );
+
+    // we should never reach this branch of code
+    if (!gasCoinInput) {
+      throw new Error('Gas coin input not found');
+    }
+
+    const witnessIndex = gasCoinInput.witnessIndex;
+    transaction.witnesses[witnessIndex] = signature;
+
+    return transaction;
   }
 }
