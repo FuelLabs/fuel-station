ad94f647790b61e337232554bfb4f31a6dc72dda
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-01-11 22:38:04 +0530

    dev: update constants for table names

--- src/constants.ts
+++ src/constants.ts
@@ -2,16 +2,6 @@ import { envSchema } from './lib/schema/config';
 
 export const env = envSchema.parse(process.env);
 
-export const ACCOUNT_TABLE_NAME =
-  env.ENV === 'local'
-    ? 'accounts_local'
-    : env.ENV === 'testnet'
-      ? 'accounts_testnet'
-      : 'accounts_mainnet';
+export const ACCOUNT_TABLE_NAME = 'accounts';
 
-export const JOB_TABLE_NAME =
-  env.ENV === 'local'
-    ? 'jobs_local'
-    : env.ENV === 'testnet'
-      ? 'jobs_testnet'
-      : 'jobs_mainnet';
+export const JOB_TABLE_NAME = 'jobs';
